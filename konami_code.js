d623d8033d93e8db3a2d73591f6023d91798ace9
Greg Morgan <gregm9999@gmail.com>
2019-03-22 00:03:55 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,9 +12,23 @@ const codes = [
 ];
 
 function init() {
-  document.body.addEventListener("keydown", alert)
+  document.body.addEventListener("keydown", onKeyDownHandler)
+}
+let index = 0;
+function onKeyDownHandler(e) {
+  let ar=[];
+  const key = e.key;
+  if (key === codes[index]) {
+    index++;
+  } if (index === codes.length) {
+    alert();
+    index = 0;
+  }
+  }else {
+    index = 0;
+  }
 }
 
 function alert() {
-  alert "Konami Code";
+  alert ("Konami Code");
 }
